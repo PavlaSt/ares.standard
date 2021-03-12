91c9a1e57638c0cb059089cd7e8cc42713499718
PavlaSt <pavla.p.novotna@seznam.cz>
2021-03-12 15:01:56 +0100

    Update AresController.java
    Fields and constructor created

--- ares.standard/src/main/java/cz/stuchlikova/ares/standard/controller/AresController.java
+++ ares.standard/src/main/java/cz/stuchlikova/ares/standard/controller/AresController.java
@@ -24,13 +24,33 @@ public class AresController {
     @Autowired
     private SoapClient soapClient;
 
+
+    private ObjectFactory objectFactory;        // = new ObjectFactory();
+    private AresDotazy aresDotazy;              // = objectFactory.createAresDotazy();
+    private Dotaz dotaz;                        // = objectFactory.createDotaz();
+    private KlicovePolozky polozky;            // = objectFactory.createKlicovePolozky();
+
+    public AresController() {
+
+        objectFactory = new ObjectFactory();
+        aresDotazy = objectFactory.createAresDotazy();
+        dotaz = objectFactory.createDotaz();
+        polozky = objectFactory.createKlicovePolozky();
+
+    }
+
+
     @RequestMapping(value = "/ico", method = RequestMethod.GET)
     public List<Odpoved> showResponseIco(@RequestParam String ico) throws DatatypeConfigurationException {
-        ObjectFactory objectFactory = new ObjectFactory();
 
-        AresDotazy aresDotazy = objectFactory.createAresDotazy();
+        //objectFactory = new ObjectFactory();
 
-        //LocalDate to xmlGregorianCalendar
+        //AresDotazy aresDotazy = objectFactory.createAresDotazy();
+
+        setAresDotazy();
+
+
+       /* //LocalDate to xmlGregorianCalendar
         LocalDate date = LocalDate.now();
         GregorianCalendar gcal = GregorianCalendar.from(date.atStartOfDay(ZoneId.systemDefault()));
         XMLGregorianCalendar xcal = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
@@ -42,13 +62,13 @@ public class AresController {
         aresDotazy.setValidationXSLT("http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_request/v_1.0.0/ares_request.xsl");
         aresDotazy.setUserMail("stuchlikova.pavla@post.cz");
         aresDotazy.setAnswerNamespaceRequired("http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_answer/v_1.0.1");
-        aresDotazy.setId("ares_dotaz");
+        aresDotazy.setId("ares_dotaz");*/
+        //Dotaz dotaz = objectFactory.createDotaz();
 
-        Dotaz dotaz = objectFactory.createDotaz();
-        dotaz.setPomocneID(1);
-        dotaz.setTypVyhledani(AresVyberTyp.FREE);
+//        dotaz.setPomocneID(1);
+//        dotaz.setTypVyhledani(AresVyberTyp.FREE);
 
-        KlicovePolozky polozky = objectFactory.createKlicovePolozky();
+        //KlicovePolozky polozky = objectFactory.createKlicovePolozky();
         polozky.setICO(ico);
 
         dotaz.setKlicovePolozky(polozky);
@@ -61,12 +81,51 @@ public class AresController {
         return response.getOdpoved();
 
 
-    }    @RequestMapping(value = "/firma", method = RequestMethod.GET)
+    }
+
+    @RequestMapping(value = "/firma", method = RequestMethod.GET)
     public List<Odpoved> showResponseFirma(@RequestParam String firma) throws DatatypeConfigurationException {
-        ObjectFactory objectFactory = new ObjectFactory();
+        //ObjectFactory objectFactory = new ObjectFactory();
 
-        AresDotazy aresDotazy = objectFactory.createAresDotazy();
+        //AresDotazy aresDotazy = objectFactory.createAresDotazy();
+        setAresDotazy();
+        /*
+        //LocalDate to xmlGregorianCalendar
+        LocalDate date = LocalDate.now();
+        GregorianCalendar gcal = GregorianCalendar.from(date.atStartOfDay(ZoneId.systemDefault()));
+        XMLGregorianCalendar xcal = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
 
+        aresDotazy.setDotazDatumCas(xcal);
+        aresDotazy.setDotazPocet(1);
+        aresDotazy.setDotazTyp(AresDotazTyp.STANDARD);
+        aresDotazy.setVystupFormat(VystupFormat.XML);
+        aresDotazy.setValidationXSLT("http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_request/v_1.0.0/ares_request.xsl");
+        aresDotazy.setUserMail("stuchlikova.pavla@post.cz");
+        aresDotazy.setAnswerNamespaceRequired("http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_answer/v_1.0.1");
+        aresDotazy.setId("ares_dotaz");
+*/
+        //Dotaz dotaz = objectFactory.createDotaz();
+//        dotaz.setPomocneID(1);
+//        dotaz.setTypVyhledani(AresVyberTyp.FREE);
+        dotaz.setMaxPocet(100);
+
+        //KlicovePolozky polozky = objectFactory.createKlicovePolozky();
+
+        polozky.setObchodniFirma(firma);
+
+        dotaz.setKlicovePolozky(polozky);
+
+        aresDotazy.getDotaz().add(dotaz);
+
+        AresOdpovedi response = soapClient.getAresOdpovedi(
+                "http://wwwinfo.mfcr.cz/cgi-bin/ares/xar.cgi", aresDotazy);
+
+        return response.getOdpoved();
+
+
+    }
+
+    public void setAresDotazy() throws DatatypeConfigurationException {
         //LocalDate to xmlGregorianCalendar
         LocalDate date = LocalDate.now();
         GregorianCalendar gcal = GregorianCalendar.from(date.atStartOfDay(ZoneId.systemDefault()));
@@ -81,23 +140,7 @@ public class AresController {
         aresDotazy.setAnswerNamespaceRequired("http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_answer/v_1.0.1");
         aresDotazy.setId("ares_dotaz");
 
-        Dotaz dotaz = objectFactory.createDotaz();
         dotaz.setPomocneID(1);
         dotaz.setTypVyhledani(AresVyberTyp.FREE);
-        dotaz.setMaxPocet(100);
-
-        KlicovePolozky polozky = objectFactory.createKlicovePolozky();
-        polozky.setObchodniFirma(firma);
-
-        dotaz.setKlicovePolozky(polozky);
-
-        aresDotazy.getDotaz().add(dotaz);
-
-        AresOdpovedi response = soapClient.getAresOdpovedi(
-                "http://wwwinfo.mfcr.cz/cgi-bin/ares/xar.cgi", aresDotazy);
-
-        return response.getOdpoved();
-
-
     }
 }
