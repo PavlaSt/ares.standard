ebc928cf7b79e05be1b0e4e3329c9720c5f4e32f
PavlaSt <pavla.p.novotna@seznam.cz>
2021-03-28 11:58:15 +0200

    Tests added

---
+++ ares.standard/src/test/java/cz/stuchlikova/ares/standard/controller/AresControllerIntegrationTest.java
@@ -0,0 +1,39 @@
+package cz.stuchlikova.ares.standard.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class AresControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getResponseByIco() {
+        ResponseEntity<List> response =
+                this.restTemplate.getForEntity("http://localhost:" + port + "/ico/?ico=03603041", List.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+
+    }
+
+    @Test
+    void getResponseByFirmName() {
+    }
+}
---
+++ ares.standard/src/test/java/cz/stuchlikova/ares/standard/service/AresOdpovediServiceTest.java
@@ -0,0 +1,26 @@
+package cz.stuchlikova.ares.standard.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AresOdpovediServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getDtoResponseByIco() {
+    }
+
+    @Test
+    void getDtoResponseByFirmName() {
+    }
+}
