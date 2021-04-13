4aa4b634173401c1d13cc0501c351bba55cff2e1
PavlaSt <pavla.p.novotna@seznam.cz>
2021-04-13 18:40:09 +0200

    Lambdas changed to method references, part of code taken out to privat method getInformation.

--- ares.standard/src/main/java/cz/stuchlikova/ares/standard/service/Transformation.java
+++ ares.standard/src/main/java/cz/stuchlikova/ares/standard/service/Transformation.java
@@ -9,31 +9,31 @@ import cz.stuchlikova.ares.standard.stub.Zaznam;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Transformation {
 
     public List<AresResponseDto> transformResponseToDto(List<Odpoved> responses) {
-        List<AresResponseDto> responseDtos = new ArrayList<>();
 
-        responses.stream()
-                .map(odpoved -> odpoved.getZaznam())
-                .flatMap(zaznams -> zaznams.stream())
-                .forEach(record -> {
-                    String obchodniFirma = record.getObchodniFirma();
-                    String ico = record.getICO();
-                    Identifikace identifikace = record.getIdentifikace();
-                    AdresaARES2 adresa = identifikace.getAdresaARES();
-                    String nazevObce = adresa.getNazevObce();
-                    String nazevCastiObce = adresa.getNazevCastiObce();
-                    String nazevUlice = adresa.getNazevUlice();
-                    Integer cisloDomovni = adresa.getCisloDomovni();
-                    String cisloOrientacni = adresa.getCisloOrientacni();
-                    String psc = adresa.getPSC();
+        return responses.stream()
+                .map(Odpoved::getZaznam)
+                .flatMap(Collection::stream)
+                .map(this::getInformation)
+                .collect(Collectors.toList());
+    }
+    private AresResponseDto getInformation(Zaznam record) {
+        String obchodniFirma = record.getObchodniFirma();
+        String ico = record.getICO();
+        Identifikace identifikace = record.getIdentifikace();
+        AdresaARES2 adresa = identifikace.getAdresaARES();
+        String nazevObce = adresa.getNazevObce();
+        String nazevCastiObce = adresa.getNazevCastiObce();
+        String nazevUlice = adresa.getNazevUlice();
+        Integer cisloDomovni = adresa.getCisloDomovni();
+        String cisloOrientacni = adresa.getCisloOrientacni();
+        String psc = adresa.getPSC();
 
-                    AresResponseDto responseDto = new AresResponseDto(obchodniFirma, ico, nazevUlice, cisloDomovni,
-                            cisloOrientacni, psc, nazevObce, nazevCastiObce);
-                    responseDtos.add(responseDto);
-                });
-        return responseDtos;
+        return new AresResponseDto(obchodniFirma, ico, nazevUlice, cisloDomovni,
+                cisloOrientacni, psc, nazevObce, nazevCastiObce);
     }
 }
