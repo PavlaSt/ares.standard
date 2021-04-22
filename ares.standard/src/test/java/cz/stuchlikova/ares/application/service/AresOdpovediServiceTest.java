7da426fdedccfcdc73f1795ed4bc19a488aace24
PavlaSt <pavla.p.novotna@seznam.cz>
2021-04-22 17:27:04 +0200

    some classes renamed

--- ares.standard/src/test/java/cz/stuchlikova/ares/application/service/AresOdpovediServiceTest.java
+++ ares.standard/src/test/java/cz/stuchlikova/ares/application/service/AresOdpovediServiceTest.java
@@ -1,12 +1,12 @@
 package cz.stuchlikova.ares.application.service;
 
-import cz.stuchlikova.ares.application.domain.AresResponseDto;
+import cz.stuchlikova.ares.application.domain.AresStandardResponseDto;
+import cz.stuchlikova.ares.application.exceptions.RecordNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.validation.ConstraintViolationException;
 import javax.xml.datatype.DatatypeConfigurationException;
 
 import java.util.List;
@@ -17,24 +17,23 @@ import static org.hamcrest.Matchers.*;
 @SpringBootTest
 class AresOdpovediServiceTest {
 
-   @Autowired
-   AresOdpovediService service;
+    @Autowired
+    AresOdpovediService service;
 
     @Test
     void getDtoResponseByIco_happy_path() throws DatatypeConfigurationException {
-        List<AresResponseDto> dtos = service.getDtoResponseByIco("27074358");
+        List<AresStandardResponseDto> dtos = service.getDtoResponseByIco("27074358");
 
         assertThat(dtos.size(), equalTo(1));
         assertThat(dtos.get(0).getObchodniFirma(), equalTo("Asseco Central Europe, a.s."));
         assertThat(dtos.get(0).getIco(), equalTo("27074358"));
     }
 
-    //TODO
     @Test
-    void getDtoResponseByIco_non_existing() throws DatatypeConfigurationException {
+    void getDtoResponseByIco_non_existing() {
 
         Assertions.assertThrows(RecordNotFoundException.class, () -> {
-             List<AresResponseDto> dtos = service.getDtoResponseByIco("12345678");
+            List<AresStandardResponseDto> dtos = service.getDtoResponseByIco("12345678");
         });
 
     }
@@ -44,21 +43,17 @@ class AresOdpovediServiceTest {
     void getDtoResponseByIco_bad_input() throws DatatypeConfigurationException {
 
         Assertions.assertThrows(ConstraintViolationException.class, () -> {
-            List<AresResponseDto> dtos = service.getDtoResponseByIco("bad_input");
+            List<AresStandardResponseDto> dtos = service.getDtoResponseByIco("bad_input");
         });
     }*/
 
-
-
     @Test
     void getDtoResponseByCompanyName() throws DatatypeConfigurationException {
-        List<AresResponseDto> dtos = service.getDtoResponseByCompanyName("Etnetera");
+        List<AresStandardResponseDto> dtos = service.getDtoResponseByCompanyName("Etnetera");
 
         assertThat(dtos.size(), equalTo(4));
         assertThat(dtos.get(0).getObchodniFirma(), containsStringIgnoringCase("Etnetera"));
         assertThat(dtos.get(0).getIco(), equalTo("24133272"));
     }
-
-    //TODO NON EXISTING
 
 }
