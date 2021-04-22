f772407a0453cbc4db3fdc4a486817c817a52249
PavlaSt <pavla.p.novotna@seznam.cz>
2021-04-22 17:29:03 +0200

    integration test changed

--- ares.standard/src/test/java/cz/stuchlikova/ares/application/controller/AresControllerIntegrationTest.java
+++ ares.standard/src/test/java/cz/stuchlikova/ares/application/controller/AresControllerIntegrationTest.java
@@ -1,43 +1,68 @@
 package cz.stuchlikova.ares.application.controller;
 
+import cz.stuchlikova.ares.application.service.AresOdpovediService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@WebMvcTest(AresController.class)
 @AutoConfigureMockMvc
 public class AresControllerIntegrationTest {
 
-    @LocalServerPort
-    private int port;
 
-    @Autowired
-    private TestRestTemplate restTemplate;
 
-    @Test
-    public void getResponseByIco() {
-        ResponseEntity<List> response =
-                this.restTemplate.getForEntity("http://localhost:" + port + "/ico/?ico=27074358", List.class);
-        System.out.println(response.getBody().get(0).toString());
-        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        @Autowired
+        private MockMvc mockMvc;
 
-    }
 
-    @Test
-    void getResponseByFirmName() {
-        ResponseEntity<List> response =
-                this.restTemplate.getForEntity("http://localhost:" + port + "/firma/?firma=Etnetera", List.class);
 
-        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
-    }
+        @Test
+        public void getResponseByIco_happy_path() throws Exception {
+            mockMvc.perform(get("/ico/?ico=27074358"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(content().json("[]"));
+
+            System.out.println(content().toString());
+        }
+
+    /*@Test
+    public void getResponseByIco_non_existing() throws Exception {
+        mockMvc.perform(get("/ico/?ico=12345678"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")))
+                .andExpect(content().string("Something happened: There are no records for this query"));
+        verify(service, times(0)).getDtoResponseByIco("12345678");
+    }*/
+
+        @Test
+        public void getResponseByIco_bad_input() throws Exception {
+            mockMvc.perform(get("/ico/?ico=bad_input"))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")))
+                    .andExpect(content().string("Something happened: getResponseByIco.ico.value: ICO must be of 8 digit"));
+
+        }
+
+        @Test
+        public void getResponseByFirmName() throws Exception {
+            mockMvc.perform(get("/firma/?firma=Etnetera"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(content().json("[]"));
+
+        }
+
 }
