cc9abce04df1ff89fb8458e25508b044b3494050
PavlaSt <pavla.p.novotna@seznam.cz>
2021-04-23 15:33:29 +0200

    draft of interface with method with generic argument and generic return type

--- ares.standard/src/main/java/cz/stuchlikova/ares/application/connector/AresClient.java
+++ ares.standard/src/main/java/cz/stuchlikova/ares/application/connector/AresClient.java
@@ -7,3 +7,20 @@ public interface AresClient {
     AresOdpovedi getAresResponse(AresDotazy request);
     cz.stuchlikova.ares.application.stub.rzp.AresOdpovedi getAresResponse(cz.stuchlikova.ares.application.stub.rzp.AresDotazy request);
 }
+
+
+interface AresClientP<O,D>{
+    O getAresResponse(D dotazy);
+}
+
+class AresClientImplP implements AresClientP<AresOdpovedi, AresDotazy>{
+    public AresOdpovedi getAresResponse(AresDotazy dotazy){
+        return new AresOdpovedi();
+    }
+}
+
+class AresClientRZPImpl implements AresClientP<cz.stuchlikova.ares.application.stub.rzp.AresOdpovedi, cz.stuchlikova.ares.application.stub.rzp.AresDotazy>{
+    public cz.stuchlikova.ares.application.stub.rzp.AresOdpovedi getAresResponse(cz.stuchlikova.ares.application.stub.rzp.AresDotazy dotazy){
+        return new cz.stuchlikova.ares.application.stub.rzp.AresOdpovedi();
+    }
+}
