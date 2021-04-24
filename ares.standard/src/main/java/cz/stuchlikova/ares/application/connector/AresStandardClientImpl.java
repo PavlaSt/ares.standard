58ab1eada605b5922cdb8880af49737327b92d4d
PavlaSt <pavla.p.novotna@seznam.cz>
2021-04-24 11:51:23 +0200

    separate client for ARES Standard implementing new interface using generic return type and parametr

---
+++ ares.standard/src/main/java/cz/stuchlikova/ares/application/connector/AresStandardClientImpl.java
@@ -0,0 +1,29 @@
+package cz.stuchlikova.ares.application.connector;
+
+import cz.stuchlikova.ares.application.configuration.ConfigProperties;
+import cz.stuchlikova.ares.application.stub.standard.AresDotazy;
+import cz.stuchlikova.ares.application.stub.standard.AresOdpovedi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.xml.bind.JAXB;
+import java.io.StringReader;
+
+@Component
+public
+class AresStandardClientImpl extends ClientBase implements AresClientGen<AresOdpovedi, AresDotazy> {
+
+    @Autowired
+    ConfigProperties properties;
+
+    public AresOdpovedi getAresResponse(AresDotazy request) {
+        String url = properties.getUrl();
+        String xmlRequest = marshalInputToXml(request);
+        String xmlResult = sendSourceReceiveResult(url, xmlRequest);
+        return unmarshalStringToObject(xmlResult);
+    }
+
+    private AresOdpovedi unmarshalStringToObject(String xmlResult) {
+        return JAXB.unmarshal(new StringReader(xmlResult), AresOdpovedi.class);
+    }
+}
