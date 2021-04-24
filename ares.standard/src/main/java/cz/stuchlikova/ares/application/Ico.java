984343e1e7c4b5f8d5f3e92921c74d82f6d16e8e
PavlaSt <pavla.p.novotna@seznam.cz>
2021-04-24 17:01:05 +0200

    equals ans hash method overriden

--- ares.standard/src/main/java/cz/stuchlikova/ares/application/Ico.java
+++ ares.standard/src/main/java/cz/stuchlikova/ares/application/Ico.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.validation.constraints.Pattern;
+import java.util.Objects;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class Ico {
@@ -23,5 +24,18 @@ public class Ico {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ico ico = (Ico) o;
+        return value.equals(ico.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
