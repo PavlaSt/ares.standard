a99269fa18f0819cd6fa21b5d7afee6a1b4ce004
PavlaSt <pavla.p.novotna@seznam.cz>
2021-04-25 15:11:39 +0200

    tests for RZP API added

--- ares.standard/src/test/java/cz/stuchlikova/ares/application/service/AresOdpovediServiceTest.java
+++ ares.standard/src/test/java/cz/stuchlikova/ares/application/service/AresOdpovediServiceTest.java
@@ -1,6 +1,7 @@
 package cz.stuchlikova.ares.application.service;
 
 import cz.stuchlikova.ares.application.Ico;
+import cz.stuchlikova.ares.application.domain.AresRzpResponseDto;
 import cz.stuchlikova.ares.application.domain.AresStandardResponseDto;
 import cz.stuchlikova.ares.application.exceptions.RecordNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -33,7 +34,6 @@ class AresOdpovediServiceTest {
 
     @Test
     void getDtoResponseByIco_non_existing() {
-
         Assertions.assertThrows(RecordNotFoundException.class, () -> {
             List<AresStandardResponseDto> dtos = service.getDtoResponseByIco(new Ico("12345678"));
         });
@@ -41,7 +41,6 @@ class AresOdpovediServiceTest {
 
     @Test
     void getDtoResponseByIco_bad_input() {
-
         Assertions.assertThrows(ConstraintViolationException.class, () -> {
             List<AresStandardResponseDto> dtos = service.getDtoResponseByIco(new Ico("bad_input"));
         });
@@ -58,12 +57,34 @@ class AresOdpovediServiceTest {
 
     @Test
     void getDtoResponseByCompanyName_non_existing() {
-
         Assertions.assertThrows(RecordNotFoundException.class, () -> {
             List<AresStandardResponseDto> dtos = service.getDtoResponseByCompanyName("QQQQQ");
         });
     }
 
 
+    @Test
+    void getDtoRzpResponseByIco_happy_path() throws DatatypeConfigurationException {
+        List<AresRzpResponseDto> dtos = service.getDtoRzpResponseByIco(new Ico("27074358"));
+
+        assertThat(dtos.get(0).getZivnosti().size(), equalTo(26));
+        assertThat(dtos.get(0).getZivnosti().get(0).getPredmetPodnikani(), containsStringIgnoringCase("Výroba, obchod a služby neuvedené v přílohách 1 až 3 živnostenského zákona"));
+        assertThat(dtos.get(0).getObchodniFirma(), equalTo("Asseco Central Europe, a.s."));
+        assertThat(dtos.get(0).getIco(), equalTo("27074358"));
+    }
+
+    @Test
+    void getDtoRzpResponseByIco_non_existing() {
+        Assertions.assertThrows(RecordNotFoundException.class, () -> {
+            List<AresRzpResponseDto> dtos = service.getDtoRzpResponseByIco(new Ico ("12345678"));
+        });
+    }
+
+    @Test
+    void getDtoRzpResponseByIco_bad_input() {
+        Assertions.assertThrows(ConstraintViolationException.class, () -> {
+            List<AresRzpResponseDto> dtos = service.getDtoRzpResponseByIco(new Ico ("bad_input"));
+        });
+    }
 
 }
