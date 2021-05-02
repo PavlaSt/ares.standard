8028b8d39c19c3de140b357df8aee90407151658
PavlaSt <pavla.p.novotna@seznam.cz>
2021-05-02 16:35:59 +0200

    response status changed from bad request to not found

--- ares.standard/src/main/java/cz/stuchlikova/ares/application/exceptions/RecordNotFoundException.java
+++ ares.standard/src/main/java/cz/stuchlikova/ares/application/exceptions/RecordNotFoundException.java
@@ -2,11 +2,13 @@ package cz.stuchlikova.ares.application.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class RecordNotFoundException extends RuntimeException  {
     public RecordNotFoundException(String message) {
         super(message);
+
     }
 
 }
