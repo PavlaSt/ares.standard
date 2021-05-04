bb9314a4ed8eceb802b622c930c3f39a92d629b3
PavlaSt <pavla.p.novotna@seznam.cz>
2021-05-04 11:58:54 +0200

    FileNotFoundException

--- ares.standard/src/main/java/cz/stuchlikova/ares/application/exceptions/GlobalExceptionHandler.java
+++ ares.standard/src/main/java/cz/stuchlikova/ares/application/exceptions/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
+import java.io.FileNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,6 +19,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RecordNotFoundException.class)
     public ResponseEntity<String> notFoundError(Exception ex) {
         return new ResponseEntity<>("Something happened: " + ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public ResponseEntity<String> fileNotFoundError(Exception ex) {
+        return new ResponseEntity<>("Something happened: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
