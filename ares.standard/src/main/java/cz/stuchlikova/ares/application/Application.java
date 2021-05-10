ecd4075676f0ae883c9f4d73419352b90524b8f1
PavlaSt <pavla.p.novotna@seznam.cz>
2021-05-10 12:43:57 +0200

    variant where the parameters(lower and upper limit, earlier and later time) are part of the inner class AresClientProperties commented

--- ares.standard/src/main/java/cz/stuchlikova/ares/application/Application.java
+++ ares.standard/src/main/java/cz/stuchlikova/ares/application/Application.java
@@ -2,12 +2,15 @@ package cz.stuchlikova.ares.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@ConfigurationPropertiesScan("cz.stuchlikova.ares.application.configuration")
 public class Application {
 
 	public static void main(String[] args) {
