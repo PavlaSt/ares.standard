82a45cd7c4eb3e89acba74083b69a1a571b674e7
PavlaSt <pavla.p.novotna@seznam.cz>
2021-05-14 21:58:41 +0200

    impors optimized

--- ares.standard/src/main/java/cz/stuchlikova/ares/application/Application.java
+++ ares.standard/src/main/java/cz/stuchlikova/ares/application/Application.java
@@ -2,7 +2,6 @@ package cz.stuchlikova.ares.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
