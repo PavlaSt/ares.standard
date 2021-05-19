07969cce6d65778c8c8bf5e4ed0247043d83b4d6
PavlaSt <pavla.p.novotna@seznam.cz>
2021-05-19 11:38:27 +0200

    If ICO doesnt comply control alghorithm MyAbstractException is thrown

--- ares.standard/src/main/java/cz/stuchlikova/ares/application/exceptions/MyAbstractException.java
+++ ares.standard/src/main/java/cz/stuchlikova/ares/application/exceptions/MyAbstractException.java
@@ -3,7 +3,7 @@ package cz.stuchlikova.ares.application.exceptions;
 import org.springframework.http.HttpStatus;
 
 public class MyAbstractException extends RuntimeException{
-    private final ErrorDescription description;
+    private final ErrorDescription errorDescription;
 
     public static class ErrorDescription {
         private final HttpStatus status;
@@ -25,10 +25,10 @@ public class MyAbstractException extends RuntimeException{
 
     public MyAbstractException(HttpStatus status, String message) {
         super(message);
-        this.description = new ErrorDescription(status, message);
+        this.errorDescription = new ErrorDescription(status, message);
     }
 
-    public ErrorDescription getDescription() {
-        return description;
+    public ErrorDescription getErrorDescription() {
+        return errorDescription;
     }
 }
